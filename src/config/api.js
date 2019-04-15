01fe5509673d9a5108ba5f985d4f9df209f3a211
Johnny <740602761@qq.com>
2019-04-15 15:09:59 +0800

    添加实际ip地址

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,8 @@
 //Server root address
-var IP = "http://127.0.0.1";
-var HOST = IP + ":8080/";
+//var IP = "http://127.0.0.1";
+var IP = "http://115.231.6.43";
+//var HOST = IP + ":8080/";
+var HOST = IP + ":8111/";
 var DOWNLOADPATH = IP + "/oll/";
 var SOCKECT_ADDR = "ws://127.0.0.1:9998";//CRHService IP address(Windows service)
 var SYSTEMNAME = "SinSim流程管理系统";
